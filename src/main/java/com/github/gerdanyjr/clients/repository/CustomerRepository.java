1102f9a107f81c971ded7cacacd28e1cffd51841
GerdanyJr <asa_lindo@hotmail.com>
2024-10-03 11:29:09 -0300

    feat: implement findByCpf on customerRepository

--- src/main/java/com/github/gerdanyjr/clients/repository/CustomerRepository.java
+++ src/main/java/com/github/gerdanyjr/clients/repository/CustomerRepository.java
@@ -1,9 +1,10 @@
 package com.github.gerdanyjr.clients.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.gerdanyjr.clients.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
+    Optional<Customer> findByCpf(String cpf);
 }
