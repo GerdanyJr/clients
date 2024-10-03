5440ed4280c30d5fd2519246da069e03d88aead8
GerdanyJr <asa_lindo@hotmail.com>
2024-10-02 23:32:11 -0300

    initial commit

---
+++ src/test/java/com/github/gerdanyjr/clients/ClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
