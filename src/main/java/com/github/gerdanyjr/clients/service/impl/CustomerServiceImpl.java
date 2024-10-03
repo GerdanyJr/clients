72fc7eaee877fb536cb47380585b4462d64e9495
GerdanyJr <asa_lindo@hotmail.com>
2024-10-03 15:14:16 -0300

    feat: add cpf validation to CustomerService

--- src/main/java/com/github/gerdanyjr/clients/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/github/gerdanyjr/clients/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.github.gerdanyjr.clients.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.github.gerdanyjr.clients.exception.ConflictException;
 import com.github.gerdanyjr.clients.model.Customer;
 import com.github.gerdanyjr.clients.repository.CustomerRepository;
 import com.github.gerdanyjr.clients.service.CustomerService;
@@ -16,6 +18,11 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer createCustomer(Customer customer) {
+        Optional<Customer> foundByCpf = customerRepository.findByCpf(customer.getCpf());
+        foundByCpf.ifPresent((c) -> {
+            throw new ConflictException("Customer with cpf: " + customer.getCpf() + " already exists!");
+        });
+
         return customerRepository.save(customer);
     }
 
