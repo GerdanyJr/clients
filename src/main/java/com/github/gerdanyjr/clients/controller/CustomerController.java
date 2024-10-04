1ee917129a420a0d27d1e2bd175bab65a62be116
GerdanyJr <asa_lindo@hotmail.com>
2024-10-03 21:19:40 -0300

    feat: implement create customer on CustomerController

---
+++ src/main/java/com/github/gerdanyjr/clients/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.github.gerdanyjr.clients.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.gerdanyjr.clients.model.Customer;
+import com.github.gerdanyjr.clients.service.CustomerService;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> create(@RequestBody Customer customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+    }
+}
