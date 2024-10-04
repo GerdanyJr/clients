75004253a260ffe8aeb431fc243383ffde18fe21
GerdanyJr <asa_lindo@hotmail.com>
2024-10-04 16:40:26 -0300

    feat: add exception handler for NotFoundException

---
+++ src/main/java/com/github/gerdanyjr/clients/exception/Handler.java
@@ -0,0 +1,14 @@
+package com.github.gerdanyjr.clients.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class Handler {
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<Void> conflictExceptionHandler(ConflictException e) {
+        return ResponseEntity.status(409).build();
+    }
+}
