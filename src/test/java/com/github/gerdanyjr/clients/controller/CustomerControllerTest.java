c38fb3e2f639e18b93fd9ab9574ff483c0b2286f
GerdanyJr <asa_lindo@hotmail.com>
2024-10-04 16:39:56 -0300

    test: create test for CustomerController create fail case

--- src/test/java/com/github/gerdanyjr/clients/controller/CustomerControllerTest.java
+++ src/test/java/com/github/gerdanyjr/clients/controller/CustomerControllerTest.java
@@ -3,7 +3,6 @@ package com.github.gerdanyjr.clients.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,48 +13,65 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.gerdanyjr.clients.exception.ConflictException;
 import com.github.gerdanyjr.clients.model.Customer;
 import com.github.gerdanyjr.clients.service.CustomerService;
 
 @WebMvcTest
 public class CustomerControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Autowired
+        private ObjectMapper objectMapper;
 
-    @MockBean
-    private CustomerService customerService;
+        @MockBean
+        private CustomerService customerService;
 
-    private Customer customer;
+        private Customer customer;
 
-    @BeforeEach
-    void setup() {
-        customer = new Customer(
-                "First",
-                "Last",
-                "123456789",
-                "test@email.com");
-    }
+        @BeforeEach
+        void setup() {
+                customer = new Customer(
+                                "First",
+                                "Last",
+                                "123456789",
+                                "test@email.com");
+        }
 
-    @DisplayName("Should return registered Customer when a valid Customer is passed")
-    @Test
-    void givenValidCustomer_whenCreateCustomer_thenReturnCustomer() throws JsonProcessingException, Exception {
-        when(customerService
-                .createCustomer(any(Customer.class)))
-                .thenAnswer(answer -> answer.getArgument(0));
+        @DisplayName("Should return registered Customer when a valid Customer is passed")
+        @Test
+        void givenValidCustomer_whenCreateCustomer_thenReturnCustomer() throws JsonProcessingException, Exception {
+                when(customerService
+                                .createCustomer(any(Customer.class)))
+                                .thenAnswer(answer -> answer.getArgument(0));
 
-        ResultActions response = mockMvc.perform(post("/customers")
-                .content(objectMapper.writeValueAsString(customer))
-                .contentType(MediaType.APPLICATION_JSON));
+                ResultActions response = mockMvc.perform(post("/customers")
+                                .content(objectMapper.writeValueAsString(customer))
+                                .contentType(MediaType.APPLICATION_JSON));
 
-        response
-                .andExpect(status().is(201))
-                .andExpect(jsonPath("$.firstName")
-                        .value(customer.getFirstName()));
-    }
+                response
+                                .andExpect(MockMvcResultMatchers.status().is(201))
+                                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName")
+                                                .value(customer.getFirstName()));
+        }
+
+        @DisplayName("Should return a 409 status code when invalid customer is passed")
+        @Test
+        void givenExistingCustomer_whenCreateCustomer_thenReturn409() throws JsonProcessingException, Exception {
+                when(customerService
+                                .createCustomer(any(Customer.class)))
+                                .thenThrow(ConflictException.class);
+
+                ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("/customers")
+                                .content(objectMapper.writeValueAsString(customer))
+                                .contentType(MediaType.APPLICATION_JSON));
+
+                response.andExpect(MockMvcResultMatchers.status().is(409));
+        }
 }
